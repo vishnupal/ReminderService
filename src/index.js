2a9eae3de117c07c270f9db915663130c513b3e5
vishnupal <vishnupalgehlot@gmail.com>
2023-03-20 22:44:02 +0530

    Create channel rabbitmq and subscribe channel

--- src/index.js
+++ src/index.js
@@ -2,24 +2,25 @@ const express = require('express');
 const app = express();
 
 const { PORT } = require('./config/serverConfig');
-const { sendBasicEmail } = require('./services/email-service');
+const { createChannel, subscribeMessage } = require('./utils/messageQueue');
+const { REMINDER_BINDING_KEY } = require('./config/serverConfig');
 
+const EmailService = require('./services/email-service');
 const TicketController = require('./controllers/ticket-controller');
 const jobs = require('./utils/job');
 
-const setupAndStartServer = () => {
+const setupAndStartServer = async () => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
+
+  const channel = await createChannel();
+  subscribeMessage(channel, EmailService.subscribeEvents, REMINDER_BINDING_KEY);
+  // subscribeMessage(channel, undefined, REMINDER_BINDING_KEY);
+
   app.post('/api/v1/tickets', TicketController.create);
   app.listen(PORT, () => {
     console.log(`Server Started on port: ${PORT}`);
-    jobs();
-    // sendBasicEmail(
-    //   'vishal@admin.com',
-    //   'vishalranjan147@gmail.com',
-    //   'This is testing email',
-    //   'hey,how are you , I hope you like the support'
-    // );
+    // jobs();
   });
 };
 
